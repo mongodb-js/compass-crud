bcfac94ae137c2df316a255d0b2fba39cc2a2ffc
Peter Schmidt <peter@peterjs.com>
2017-08-22 14:09:24 +1000

    Also npm link in react-bootstrap for Clone modal?

--- scripts/link.sh
+++ scripts/link.sh
@@ -5,9 +5,13 @@ cd ${COMPASS_HOME}/node_modules/hadron-app
 npm link
 cd ${COMPASS_HOME}/node_modules/react
 npm link
+cd ${COMPASS_HOME}/node_modules/react-bootstrap
+npm link
+npm link ../react
 cd ${PLUGIN_DIR}
 npm link hadron-app
 npm link react
+npm link react-bootstrap
 npm link
 cd ${COMPASS_HOME}
 npm link @mongodb-js/${PLUGIN_NAME}
--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 PLUGIN_DIR=${PWD}
 PLUGIN_NAME=${PWD##*/}
-npm unlink hadron-app
+npm unlink react-bootstrap
 npm unlink react
-cd ${COMPASS_HOME}/node_modules/hadron-app
+npm unlink hadron-app
+cd ${COMPASS_HOME}/node_modules/react-bootstrap
 npm unlink
+# Not sure if this works, at this point it's usually easier to just
+# `rm -r node_modules` in plugin and Compass, and reinstall both :/
+npm unlink ../react
 cd ${COMPASS_HOME}/node_modules/react
+npm unlink
+cd ${COMPASS_HOME}/node_modules/hadron-app
 npm unlink
 cd ${COMPASS_HOME}
 npm unlink @mongodb-js/${PLUGIN_NAME}
