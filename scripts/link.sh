3d853956df69bf44f9e52070db4632163566697a
Peter Schmidt <peter@peterjs.com>
2017-08-22 14:17:59 +1000

    Revert "Also npm link in react-bootstrap for Clone modal?"
    This reverts commit 6304e7380b68a1de1a59a3fcace6eed4ba8ac6c2.

--- scripts/link.sh
+++ scripts/link.sh
@@ -5,13 +5,9 @@ cd ${COMPASS_HOME}/node_modules/hadron-app
 npm link
 cd ${COMPASS_HOME}/node_modules/react
 npm link
-cd ${COMPASS_HOME}/node_modules/react-bootstrap
-npm link
-npm link ../react
 cd ${PLUGIN_DIR}
 npm link hadron-app
 npm link react
-npm link react-bootstrap
 npm link
 cd ${COMPASS_HOME}
 npm link @mongodb-js/${PLUGIN_NAME}
--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 PLUGIN_DIR=${PWD}
 PLUGIN_NAME=${PWD##*/}
-npm unlink react-bootstrap
-npm unlink react
 npm unlink hadron-app
-cd ${COMPASS_HOME}/node_modules/react-bootstrap
-npm unlink
-# Not sure if this works, at this point it's usually easier to just
-# `rm -r node_modules` in plugin and Compass, and reinstall both :/
-npm unlink ../react
-cd ${COMPASS_HOME}/node_modules/react
-npm unlink
+npm unlink react
 cd ${COMPASS_HOME}/node_modules/hadron-app
+npm unlink
+cd ${COMPASS_HOME}/node_modules/react
 npm unlink
 cd ${COMPASS_HOME}
 npm unlink @mongodb-js/${PLUGIN_NAME}
